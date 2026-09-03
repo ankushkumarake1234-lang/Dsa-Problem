0ccd00683dbaff867b12d9c3170d6a09bb035e63
ANKUSH KUMAR <ankushkumarake1234@gmail.com>
2026-09-03 16:12:29 +0530

    Time: 1 ms (93.24%), Space: 45.2 MB (63.12%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -3,13 +3,23 @@ import java.util.*;
 import java.io.*;
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        for(int i = 1; i<n; i=i+3){
-            if(nums[i] != nums[i-1]){
-                return nums[i-1];
-            }
+    //     int n = nums.length;
+    //     Arrays.sort(nums);
+    //     for(int i = 1; i<n; i=i+3){
+    //         if(nums[i] != nums[i-1]){
+    //             return nums[i-1];
+    //         }
+    //     }
+    //     return nums[n-1];
+    // }
+
+        //    this is done by bit multiplution method 
+        int ones = 0;
+        int twos = 0;
+        for(int i = 0; i<nums.length; i++){
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
         }
-        return nums[n-1];
+        return ones;
     }
 }
