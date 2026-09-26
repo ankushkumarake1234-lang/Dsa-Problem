92deebc01712c72b999e97494ad2f59caf6c1ef0
ANKUSH KUMAR <ankushkumarake1234@gmail.com>
2026-09-26 10:42:42 +0530

    Time: 1 ms (99.67%), Space: 42.8 MB (32.22%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -2,34 +2,27 @@ import java.lang.*;
 import java.util.*;
 class Solution {
     public int divide(int dividend, int divisor) {
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
-             return Integer.MAX_VALUE;
+        if(dividend == Integer.MIN_VALUE && divisor == -1){
+           return Integer.MAX_VALUE;
         }
-        // int ans = 0;
-        // while (dividend >= divisor) {
-        //     dividend = dividend - divisor;
-        //     ans++;
-        // }
-        // return ans;
-
-        boolean negative = (dividend < 0) ^ (divisor < 0);
-        long x = Math.abs((long) dividend); 
-        long y = Math.abs((long) divisor);
-
-        long ans = 0;
-        while(x >= y){
-         long a = y;
-         long multi = 1;
-         while(x >= (a << 1)){
-            a <<= 1;
-            multi <<=1;
-         }
-         x -= a;
-         ans += multi;
+        boolean negative = (dividend < 0) ^ ( divisor < 0);
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+        long quotient = 0;
+        while(a >= b){
+            long temp = b;
+            long multiple = 1;
+            while( a>= (temp << 1)){
+                temp = temp << 1;
+                multiple = multiple << 1;
+            }
+            a = a - temp;
+            quotient = quotient + multiple;
         }
         if(negative){
-            ans = -ans;
+            quotient = -quotient;
         }
-        return (int) ans;
+        return (int) quotient;
+
     }
 }
